51ef38182c15bc10b2b24f3184e9b8ec51b9fe46
dsandersAzure <dsanders_can@outlook.com>
2017-05-15 22:08:42 -0400

    Promote to version 1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python_cowbull_game',
-      version='0.4.1',
+      version='1.0',
       description='Python cowbull game object',
       url='https://github.com/dsandersAzure/python_cowbull_game',
       author='David Sanders',
